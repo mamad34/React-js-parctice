8db221c32e73811faef3df9a0f4151547d9e35e4
mohammad <mohammadtt77@yahoo.com>
2021-10-01 15:30:29 +0330

    episode 11 season 6

--- react3/react3/src/SeasonDisplay.js
+++ react3/react3/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import "./SeasonDisplay.css";
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "lets's hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr it is cold",
+    iconName: "snowflake",
+  },
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -11,9 +23,17 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   console.log(season);
-
   console.log(props.lat);
-  return <div>Season Display </div>;
+
+  const { iconName, text } = seasonConfig[season]; //{text , iconName}
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`iconLeft massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`iconRight massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
